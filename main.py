590d522a521b1c1a0352eff2d4ef43aeec18c7b2
Evgeniy <vigi73@mail.ru>
2017-02-28 18:13:01 +0700

    v5

--- main.py
+++ main.py
@@ -14,26 +14,25 @@ if __name__ == '__main__':
     st_time = datetime.now()
 
     for i, inn in enumerate(inns_mb, start=1):
-        value1, value2, name_org = get_value(inn.split()[0])
+        value1, value2, name_org = get_value(inn)
 
-        if inn.split()[1] == 'm':
-            urls = [f'http://bus.gov.ru/public/agency/agency_tasks.json?agency={value1}&task={value2}',
-                    f'http://bus.gov.ru/public/agency/last-agency-plan.json?agency={value1}&d-5460-o=2&d-5460-s=1',
-                    f'http://bus.gov.ru/public/agency/last-operation.json?agency={value1}&stage=',
-                    f'http://bus.gov.ru/public/agency/last-annual-balance-F0503721-info.json?agencyId={value1}',
-                    f'http://bus.gov.ru/public/agency/last-annual-balance-F0503730-info.json?agencyId={value1}',
-                    f'http://bus.gov.ru/public/annual-balance-f0503737/show-last-annual-balance.json?agencyId={value1}',
-                    f'http://bus.gov.ru/public/agency/last_activity_estate_usage_report.json?agency={value1}',
-                    f'http://bus.gov.ru/public/agency/last-measure-details.json?agency={value1}'
-                    ]
+        urls = [f'http://bus.gov.ru/public/agency/agency_tasks.json?agency={value1}&task={value2}',
+                f'http://bus.gov.ru/public/agency/last-agency-plan.json?agency={value1}&d-5460-o=2&d-5460-s=1',
+                f'http://bus.gov.ru/public/agency/last-operation.json?agency={value1}&stage=',
+                f'http://bus.gov.ru/public/agency/last-annual-balance-F0503721-info.json?agencyId={value1}',
+                f'http://bus.gov.ru/public/agency/last-annual-balance-F0503730-info.json?agencyId={value1}',
+                f'http://bus.gov.ru/public/annual-balance-f0503737/show-last-annual-balance.json?agencyId={value1}',
+                f'http://bus.gov.ru/public/agency/last_activity_estate_usage_report.json?agency={value1}',
+                f'http://bus.gov.ru/public/agency/last-measure-details.json?agency={value1}'
+                       ]
 
-        result = pars(urls, inn.split()[0])
+
+        result = pars(urls, inn)
         buh = sum([result[3], result[4], result[5]])
 
         pr = sum(result) * 100 / 8
         get_print(i, name_org, result, pr, "+" if buh == 3 else "")
         matrix.append(result)
-
 
     end_time = datetime.now()
 
