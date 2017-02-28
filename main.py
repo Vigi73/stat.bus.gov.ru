448b9ae35a879b5d9cadfec9db285e06cd27406f
Vigi73 <vigi73@mail.ru>
2017-02-28 16:50:20 +0700

    v4

--- main.py
+++ main.py
@@ -14,17 +14,18 @@ if __name__ == '__main__':
     st_time = datetime.now()
 
     for i, inn in enumerate(inns_mb, start=1):
-        value1, value2, name_org = get_value(inn)
+        value1, value2, name_org = get_value(inn.split(',')[0])
 
-        urls = [f'http://bus.gov.ru/public/agency/agency_tasks.json?agency={value1}&task={value2}',
-                f'http://bus.gov.ru/public/agency/last-agency-plan.json?agency={value1}&d-5460-o=2&d-5460-s=1',
-                f'http://bus.gov.ru/public/agency/last-operation.json?agency={value1}&stage=',
-                f'http://bus.gov.ru/public/agency/last-annual-balance-F0503721-info.json?agencyId={value1}',
-                f'http://bus.gov.ru/public/agency/last-annual-balance-F0503730-info.json?agencyId={value1}',
-                f'http://bus.gov.ru/public/annual-balance-f0503737/show-last-annual-balance.json?agencyId={value1}',
-                f'http://bus.gov.ru/public/agency/last_activity_estate_usage_report.json?agency={value1}',
-                f'http://bus.gov.ru/public/agency/last-measure-details.json?agency={value1}'
-                ]
+        if inn.split(','[1]) == 'm':
+            urls = [f'http://bus.gov.ru/public/agency/agency_tasks.json?agency={value1}&task={value2}',
+                    f'http://bus.gov.ru/public/agency/last-agency-plan.json?agency={value1}&d-5460-o=2&d-5460-s=1',
+                    f'http://bus.gov.ru/public/agency/last-operation.json?agency={value1}&stage=',
+                    f'http://bus.gov.ru/public/agency/last-annual-balance-F0503721-info.json?agencyId={value1}',
+                    f'http://bus.gov.ru/public/agency/last-annual-balance-F0503730-info.json?agencyId={value1}',
+                    f'http://bus.gov.ru/public/annual-balance-f0503737/show-last-annual-balance.json?agencyId={value1}',
+                    f'http://bus.gov.ru/public/agency/last_activity_estate_usage_report.json?agency={value1}',
+                    f'http://bus.gov.ru/public/agency/last-measure-details.json?agency={value1}'
+                    ]
 
         result = pars(urls, inn)
         buh = sum([result[3], result[4], result[5]])
